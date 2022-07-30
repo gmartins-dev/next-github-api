8b8daa9112f914e71d40ebf4678fd382b47ad717
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-07-30 21:55:54 +0100

    criada estrutura inicial de pastas/components

---
+++ src/components/LinksArea/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LinksArea = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default LinksArea
---
+++ src/components/TopArea/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TopArea = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default TopArea
