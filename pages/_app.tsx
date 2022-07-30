0c027fa6c818b34c713772eabda3a3d45bbf855e
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-07-30 21:45:25 +0100

    global styles add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+html {
+  font-size: 62.5%;
+  overflow-x: hidden;
+}
+body, 
+input, 
+button, 
+textarea, 
+select {
+  font-family: 'Space Mono', sans-serif;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle/>
+    </>
+  )
 }
 
 export default MyApp
