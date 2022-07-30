3fe5cfb64900a387a50e063a0abb4605b5e47bf5
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-07-30 22:07:18 +0100

    edições na pagina home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
 import type { NextPage } from 'next'
+import styled from 'styled-components'
+import TopArea from '../src/components/TopArea'
+import { ThemeContextProvider } from '../src/contexts/ThemeContext'
 
 
 const Home: NextPage = () => {
   return (
-    <h1>Teste</h1>
+    <ThemeContextProvider>
+      <Container>
+        <h1>testeeeew</h1>
+        <TopArea/>
+      </Container>
+    </ThemeContextProvider>
   )
 }
+
+const Container = styled.main`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: ${props => props.theme.colors.background};
+  padding: 3.1rem 2.4rem;
+  @media (min-width: 768px) {
+    padding: 3.1rem 7rem;
+  }
+`;
 
 export default Home
